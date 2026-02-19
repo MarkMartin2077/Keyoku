74fec7fdfddd53ecc8c65a3cacfcd7aad5e072fa
Mark Martin <menm619@gmail.com>
2026-02-19 03:18:08 -0800

    [Feature] Add monthly subscription option with free trial

--- Keyoku/Managers/Purchases/EntitlementOption.swift
+++ Keyoku/Managers/Purchases/EntitlementOption.swift
@@ -7,14 +7,17 @@
 
 enum EntitlementOption: Codable, CaseIterable {
     case yearly
-    
+    case monthly
+
     var productId: String {
         switch self {
         case .yearly:
             return "markmartin89.Keyoku.yearly"
+        case .monthly:
+            return "markmartin89.Keyoku.monthly"
         }
     }
-    
+
     static var allProductIds: [String] {
         EntitlementOption.allCases.map({ $0.productId })
     }
